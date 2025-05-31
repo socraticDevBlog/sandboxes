151fac10ab2fee89895327d41f26470926a324e1
socraticdev-on-mainframe <socraticdev@gmail.com>
2025-05-31 15:31:28 +0000

    remove unecessary local

--- terraform_cycle_management/recommended/main.tf
+++ terraform_cycle_management/recommended/main.tf
@@ -1,23 +1,18 @@
 
-# Boucle sur chaque étudiant pour générer un random_string et calculer la date de décès
-locals {
-  students = var.students
-}
-
 resource "random_string" "this" {
-  for_each = local.students
+  for_each = var.students
   length   = 6
   special  = false
 }
 
 locals {
-  dob_rfc3339   = { for k, s in local.students : k => "${s.dob}T00:00:00Z" }
-  life_end_date = { for k, s in local.students : k => timeadd(local.dob_rfc3339[k], "525600h") }
+  dob_rfc3339   = { for k, s in var.students : k => "${s.dob}T00:00:00Z" }
+  life_end_date = { for k, s in var.students : k => timeadd(local.dob_rfc3339[k], "525600h") }
 }
 
 output "students_summary" {
   value = {
-    for k, s in local.students :
+    for k, s in var.students :
     k => {
       unique_id     = "${s.first_name}-${random_string.this[k].result}"
       date_of_birth = s.dob
